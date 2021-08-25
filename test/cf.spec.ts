f61c5bc6e5d06f6a9a946012c83371ab7cf76332
Erick Saravia <erick@norttech.com>
2021-08-25 10:53:34 -0600

    refactor(test): testing organization updated

--- test/cf.spec.ts
+++ test/cf.spec.ts
@@ -8,24 +8,26 @@ beforeAll(() => {
 });
 
 describe('Hello World', () => {
-  describe('GET /', () => {
-    const endpoint = `http/simple/helloWorldHttp`;
+  const endpoint = `http/simple/helloWorldHttp`;
 
+  describe(`GET /${endpoint}`, () => {
     it('return status 200: when message is provided as query param', () => {
       return request(app).get(`/${endpoint}?message=Hello`).expect(200);
     });
 
+    it('return status 400: when message is not provided', () => {
+      console.log(endpoint);
+      return request(app).get(`/${endpoint}`).expect(400);
+    });
+  });
+
+  describe(`POST /${endpoint}`, () => {
     it('return status 200: when message is provided as body data', () => {
       return request(app)
         .post(`/${endpoint}`)
         .send({ message: 'hello' })
         .set('Accept', 'application/json')
         .expect(200);
-    });
-
-    it('return status 400: when message is not provided', () => {
-      console.log(endpoint);
-      return request(app).get(`/${endpoint}`).expect(400);
     });
   });
 });
